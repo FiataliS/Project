41f6a2aff6cddeb6647774dbe57769638d64851f
FiataliS <FiataliS@gmail.con>
2021-09-02 11:49:21 +0500

    Homework 3_1

---
+++ src/Lesson3_1/Orange.java
@@ -0,0 +1,14 @@
+package Lesson3_1;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+
+}
